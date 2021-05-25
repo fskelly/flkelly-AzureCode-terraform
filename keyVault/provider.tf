d32e9aa1948e667be0c97e7ea637d0f611963372
Fletcher Kelly <fletcher_kelly@outlook.com>
2021-05-25 09:06:30 +0000

    updated provider.tf KeyVault

--- keyVault/provider.tf
+++ keyVault/provider.tf
@@ -1,8 +1,15 @@
-provider "azurerm" {
-  version = "=2.20.0"
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.60.0"
     }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
   }
 }
